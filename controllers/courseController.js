4d0b10ea1034303199fa4ec79f24b8c0d1c91f1b
DevUmutYildirim <devumutyildirim@gmail.com>
2022-08-22 01:25:08 +0300

    Added individual course pages and slugify

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -32,3 +32,20 @@ exports.getAllCourses = async (req, res) => {
     });
   }
 };
+
+exports.getCourse = async (req, res) => {
+  try {
+    // catch the req.params.slug and define as course
+    const course = await Course.findOne({ slug: req.params.slug });
+    // render course variable to course template.
+    res.status(200).render("course", {
+      course,
+      page_name: "courses",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
--- models/Course.js
+++ models/Course.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 const Schema = mongoose.Schema;
 
-const courseSchema = new Schema({
+const CourseSchema = new Schema({
   name: {
     type: String,
     unique: true,
@@ -16,8 +17,23 @@ const courseSchema = new Schema({
     type: Date,
     default: Date.now,
   },
+  slug: {
+    type: String,
+    unique: true,
+  },
+});
+// We use slugify middleware here.
+// Before send the schema to db, we create a slug using this.name
+// and we use this slug in http://localhost300/:id as id
+// and we don't see id in the link, we see name slug.
+CourseSchema.pre("validate", function (next) {
+  this.slug = slugify(this.name, {
+    lower: true,
+    strict: true,
+  });
+  next();
 });
 
-const Course = mongoose.model("Course", courseSchema);
+const Course = mongoose.model("Course", CourseSchema);
 
 module.exports = Course;
