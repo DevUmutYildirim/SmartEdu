6e0feda45aa87dcd3462de5801647cf80bf90b3d
DevUmutYildirim <devumutyildirim@gmail.com>
2022-08-25 03:25:23 +0300

    Session added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
@@ -12,13 +13,28 @@ mongoose.connect("mongodb://localhost/smartedu-db");
 // Template Engine
 app.set("view engine", "ejs"); // set engine
 
+// Global Variable
+
+global.userIN = null;
+
 // Middlewares
 app.use(express.static("public")); // show static files
 // we need to use this middlwares to catch the data comes from the req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "my_keyboard_foz",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // Routes
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
